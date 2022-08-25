2056b00012eff43481ce5115f026c27f4a073f1d
dadiallo <d.abdoulayead28@gmail.com>
2022-08-25 17:19:00 +0000

    first commit

---
+++ src/test/java/com/odk/errornotesapi/ErrorNotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odk.errornotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorNotesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
