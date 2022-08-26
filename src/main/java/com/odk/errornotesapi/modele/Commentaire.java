178c9d529e9a155b5804c211e07ae69971c00fce
nurdev1 <fatoudevweb1@gmail.com>
2022-08-26 09:00:18 +0000

    add modeles attribut

--- src/main/java/com/odk/errornotesapi/modele/Commentaire.java
+++ src/main/java/com/odk/errornotesapi/modele/Commentaire.java
@@ -1,6 +1,19 @@
 package com.odk.errornotesapi.modele;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Setter
+@Getter
 public class Commentaire {
-    private int id_commentaire;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_commentaire;
+    private String libele;
+    private Date date;
 
 }
--- src/main/java/com/odk/errornotesapi/modele/Probleme.java
+++ src/main/java/com/odk/errornotesapi/modele/Probleme.java
@@ -1,4 +1,20 @@
 package com.odk.errornotesapi.modele;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class Probleme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   // @Column(name = "id_probleme")
+    private Long id_probleme;
+    private String descriptionProbleme;
+    private String technologie;
+    private String etat;
+
 }
