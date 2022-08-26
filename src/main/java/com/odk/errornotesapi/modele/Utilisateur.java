74b3ebd245ec59fd2c4ca890954b59a39671c868
dadiallo <d.abdoulayead28@gmail.com>
2022-08-26 09:25:18 +0000

    Add modele solution utilisateur

--- src/main/java/com/odk/errornotesapi/modele/Utilisateur.java
+++ src/main/java/com/odk/errornotesapi/modele/Utilisateur.java
@@ -1,4 +1,23 @@
 package com.odk.errornotesapi.modele;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+@Getter
+@Setter
 public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id_utilisateur;
+    String nom;
+    String prenom;
+    String email;
+    String numero;
+    String role;
+
 }
