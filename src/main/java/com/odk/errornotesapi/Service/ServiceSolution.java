5938c8edf71133a80480318635a7e98a6d50bc72
dadiallo <d.abdoulayead28@gmail.com>
2022-08-26 09:45:57 +0000

    Add service solution,Utilisateur

--- src/main/java/com/odk/errornotesapi/Service/ServiceSolution.java
+++ src/main/java/com/odk/errornotesapi/Service/ServiceSolution.java
@@ -1,4 +1,10 @@
 package com.odk.errornotesapi.Service;
 
+import com.odk.errornotesapi.modele.Commentaire;
+import com.odk.errornotesapi.modele.Solution;
+
 public interface ServiceSolution {
+    Solution soumettreSolution(Solution solution);
+    Solution Voirsolution(Solution solution);
+    Commentaire Commenter(Solution solution);
 }
