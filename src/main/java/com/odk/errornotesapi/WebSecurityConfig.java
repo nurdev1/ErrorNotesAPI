ceb5da218f3be186496d6f93099bc382efb320c1
dadiallo <d.abdoulayead28@gmail.com>
2022-08-31 13:15:18 +0000

    ajout de spring security and jwt

--- src/main/java/com/odk/errornotesapi/WebSecurityConfig.java
+++ src/main/java/com/odk/errornotesapi/WebSecurityConfig.java
@@ -1,12 +1,10 @@
 package com.odk.errornotesapi;
 
-import com.odk.errornotesapi.Service.ServiceUtilisateur;
+
 import com.odk.errornotesapi.Service.UserDetailsServiceImpl;
 import com.odk.errornotesapi.filters.JwtAuthentificationFilter;
 import com.odk.errornotesapi.filters.JwtAutorizationFilter;
-import com.odk.errornotesapi.modele.Utilisateur;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,16 +15,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.util.ArrayList;
-import java.util.Collection;
 
 @AllArgsConstructor
 @Configuration
@@ -46,6 +35,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         //http.formLogin();
         //http.authorizeRequests().antMatchers("/Utilisateur/Afficher/**").hasAuthority("ADMIN_ROLE");
         http.authorizeRequests().antMatchers(HttpMethod.POST,"/Utilisateur/refreshToken/**","/Utilisateur/Inscrire/**").permitAll();
+        http.authorizeRequests().antMatchers("/swagger-ui*/**","/ErrorNotesApi-openapi/**").permitAll();
+        http.authorizeRequests()
+                .antMatchers(
+                        "/v2/api-docs",
+                        "/swagger-resources",
+                        "/swagger-resources/configuration/ui",
+                        "/swagger-resources/configuration/security")
+                .permitAll();
         http.authorizeRequests().anyRequest().authenticated() ;
         http.addFilter(new JwtAuthentificationFilter(authenticationManagerBean()));
         http.addFilterBefore(new JwtAutorizationFilter(), UsernamePasswordAuthenticationFilter.class);
